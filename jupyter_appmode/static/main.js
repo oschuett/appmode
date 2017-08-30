05f92f89d863098f75e1eb3805816e1674be32c0
Ole Schuett <ole.schuett@empa.ch>
2017-08-30 15:28:16 +0200

    widgetarea was removed in ipywidgets 7.0

--- jupyter_appmode/static/main.js
+++ jupyter_appmode/static/main.js
@@ -20,11 +20,11 @@ define([
     //==========================================================================
     var appmode_unload_handler = function (e) {
         console.log("Appmode: running unload handler");
-        
+
         var nb = Jupyter.notebook;
         var url_parts = [nb.base_url, 'apps', nb.notebook_path];
         var url = utils.url_path_join.apply(null, url_parts);
-        
+
         // tell server to clean up session, kernel, and tmp notebook file.
         utils.ajax(url, {cache: false, type: "DELETE", async: false});
     };
@@ -32,7 +32,7 @@ define([
     //==========================================================================
     function goto_app_mode() {
         console.log("Appmode: going to app mode.");
-        
+
         if (Jupyter.notebook.dirty) {
             dialog.modal({
                 title : 'Unsaved changes.',
@@ -41,38 +41,38 @@ define([
                 notebook: Jupyter.notebook,
                 keyboard_manager: Jupyter.keyboard_manager,
             });
-            
+
         }else{
             // kill Jupyter session
             Jupyter.notebook.session.delete();
-                   
+
             // change URL
             var base_url = Jupyter.notebook.base_url;
             var prefix = base_url+"notebooks/"
-            var path = window.location.pathname.substring(prefix.length); 
+            var path = window.location.pathname.substring(prefix.length);
             window.location.pathname = base_url+"apps/"+path;
         }
     }
-    
+
     //==========================================================================
     function goto_normal_mode() {
         console.log("Appmode: going to normal mode.");
         var base_url = Jupyter.notebook.base_url;
         var prefix = base_url+"apps/"
-        var path = window.location.pathname.substring(prefix.length); 
+        var path = window.location.pathname.substring(prefix.length);
         window.location.pathname = base_url+"notebooks/"+path;
     }
-    
+
     //==========================================================================
     function kernel_inject_url(kernel) {
         console.log("Appmode: injecting url into new kernel"); 
         kernel.execute("jupyter_notebook_url = '" + window.location + "'");
     }
-    
+
     //==========================================================================
     function initialize_step1() {
         console.log("Appmode: initialize_step1");
-        
+
         if (Jupyter.notebook && Jupyter.notebook._fully_loaded) {
             console.log("Appmode: notebook already loaded.");
             initialize_step2();
@@ -86,11 +86,11 @@ define([
     function initialize_step2() {
         console.log("Appmode: initialize_step2");
         var nb = Jupyter.notebook
-        
+
         //console.log("Appmode: nb.kernel:"+nb.kernel);
         //if(nb.kernel)
         //    console.log("Appmode: nb.kernel.info_reply:"+nb.kernel.info_reply.status);
-        
+
         if(nb.kernel && nb.kernel.info_reply.status) {
             console.log("Appmode: kernel already ready.");
             initialize_step3();
@@ -103,9 +103,9 @@ define([
     //==========================================================================
     function initialize_step3() {
         console.log("Appmode: initialize_step3");
-        
+
         kernel_inject_url(Jupyter.notebook.kernel);
-        
+
         // run kernel_inject_url() again when a new kernel is created
         events.on('kernel_created.Kernel kernel_created.Session', function(event, data) {
             kernel_inject_url(data.kernel);
@@ -117,11 +117,11 @@ define([
             initialize_step4();
         }
     }
-    
+
     //==========================================================================
     function initialize_step4() {
         console.log("Appmode: initialize_step4");
-        
+
         if (Jupyter.notebook.trusted) {
             initialize_step5();
         }else{
@@ -138,36 +138,15 @@ define([
     //==========================================================================
     function initialize_step5() {
         console.log("Appmode: initialize_step5");
-
-        var cells_ready = true;
-        var cells = Jupyter.notebook.get_cells();
-        for (var i = 0; i < cells.length; i++) {
-            if(cells[i].cell_type==='code' && ! cells[i].widgetarea){
-                cells_ready = false;
-                break;
-            }
-        }
-
-        if (cells_ready) {
-            initialize_step6();
-        } else {
-            console.log("Appmode: waiting for cells to get ready.");
-            setTimeout(initialize_step5, 100);
-        }
-    }
-
-    //==========================================================================
-    function initialize_step6() {
-        console.log("Appmode: initialize_step6");
         //console.log("Appmode info_reply.status: "+Jupyter.notebook.kernel.info_reply.status);
-        
+
         // disable autosave
         Jupyter.notebook.set_autosave_interval(0);
 
         // run all cells
         Jupyter.notebook.clear_all_output();
         Jupyter.notebook.execute_all_cells();
-        
+
         // disable code editing
         $('.CodeMirror').each(function() {
             this.CodeMirror.setOption('readOnly', "nocursor");
@@ -175,16 +154,16 @@ define([
 
         // install unload-handler
         window.onbeforeunload = appmode_unload_handler;
-        
+
         // register on_click handler
         $('#jupyer-appmode-leave').click(goto_normal_mode);
-        
+
         // hide loading screen
         $('#jupyter-appmode-loader').slideUp();
-        
+
         console.log("Appmode: initialization finished");
     }
-    
+
     //==========================================================================
     var load_ipython_extension = function() {
 
@@ -196,7 +175,7 @@ define([
             callback : goto_app_mode
         }]);
 
-        initialize_step1(); 
+        initialize_step1();
     };
 
     //==========================================================================
