01175727f86a4167e731adea371c54a33df4b6bd
Ole Schuett <ole.schuett@empa.ch>
2017-10-13 17:40:44 +0200

    version 0.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
     name='appmode',
     license='MIT',
-    version='0.0.2',
+    version='0.0.3',
     author = 'Ole Schuett',
     author_email = 'ole.schuett@empa.ch',
     url='http://github.com/oschuett/appmode',
