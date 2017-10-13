111913a3025ee5db7def6ef87ce998c87c99a0e4
Ole Schuett <ole.schuett@empa.ch>
2017-10-13 14:12:25 +0200

    version 0.0.1

--- appmode/__init__.py
+++ appmode/__init__.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 
-__version__ = "0.1.0"
-
 # Jupyter Extension points
 
 def _jupyter_nbextension_paths():
     return [dict(
         section="notebook",
         src="static",
-        dest="jupyter_appmode",
-        require="jupyter_appmode/main")]
+        dest="appmode",
+        require="appmode/main")]
 
 def _jupyter_server_extension_paths():
-    return [{"module":"jupyter_appmode.server_extension"}]
-    
+    return [{"module":"appmode.server_extension"}]
+
 #EOF
--- setup.py
+++ setup.py
@@ -1,46 +1,24 @@
-import os
+# -*- coding: utf-8 -*-
 
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup
-
-modulename = 'jupyter_appmode'
-the_license = "The MIT license"
-
-# Get the version number in a dirty way
-folder = os.path.split(os.path.abspath(__file__))[0]
-fname = os.path.join(folder, modulename, '__init__.py')
-with open(fname) as init:
-    ns = {}
-    # Get lines that match, remove comment part 
-    # (assuming it's not in the string...)
-    versionlines = [l.partition('#')[0] 
-                    for l in init.readlines() if l.startswith('__version__')]
-if len(versionlines) != 1:
-    raise ValueError("Unable to detect the version lines")
-versionline = versionlines[0]
-version = versionline.partition('=')[2].replace('"', '').replace("'", '').strip()
-
-description_short = "A Jupyter extension to hide the controls, disable editing, and work in a 'App mode'"
-with open(os.path.join(folder, 'README.md')) as f:
-    description_long = f.read()
+from __future__ import print_function
+from setuptools import setup
 
 setup(
-    name=modulename,
-    description=description_short,
-    url='http://github.com/oschuett/jupyter_appmode',
-    license=the_license,
+    name='appmode',
+    license='MIT',
+    version='0.0.1',
     author = 'Ole Schuett',
-    version=version,
-    install_requires=[
-        'jupyter'
-    ],
-    packages=find_packages(),
-    # Needed to include some static files declared in MANIFEST.in
-    include_package_data = True,
-    keywords = ['jupyter extension', 'app mode'],
-    long_description=description_long,
+    author_email = 'ole.schuett@empa.ch',
+    url='http://github.com/oschuett/appmode',
+    description='A Jupyter extensions that turns notebooks into web applications.',
+
+    packages=["appmode"],
+    install_requires=['notebook==5'],
+    data_files=[('share/jupyter/nbextensions/appmode', [
+                    'appmode/static/main.js',
+                    'appmode/static/gears.svg'
+    ])],
+
     classifiers=[
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
@@ -49,4 +27,11 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+
 )
+
+print("Run the following commands to enable appmode:")
+print("$ jupyter serverextension enable --py appmode")
+print("$ jupyter nbextension     enable --py appmode")
+
+#EOF
