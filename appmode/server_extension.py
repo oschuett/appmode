844a99e9fe3b7ef9edee3877887906d9ca88591f
pdstack <125979306+pdstack@users.noreply.github.com>
2023-03-10 02:40:32 +1100

    Compatibility with ServerApp backend (#65)
    Jupyter provides nbclassic mode as the compatible notebook backend that uses the jupyter sever backend. It will be fully replaced by the notebookv7 when it is released. But for the moment, if the user what to use the nbclassic mode the appmode is not working. 
    
    This commit makes it compatible with the nbclassic backend by fixing the concurrent issue mentioned in https://discourse.jupyter.org/t/jupyter-notebook-zmq-message-arrived-on-closed-channel-error/17869. It also adds options to allow controlling the path of template files of opened notebooks. 
    
    Since the jupyter server backends don't have the templates needed by the appmode, this commit explicitly adds the template path so those can be found and used.

--- appmode/server_extension.py
+++ appmode/server_extension.py
@@ -1,13 +1,27 @@
 # -*- coding: utf-8 -*-
 
 import os
+import inspect
 import itertools
 from notebook.utils import url_path_join
 from notebook.base.handlers import IPythonHandler, FilesRedirectHandler, path_regex
 import notebook.notebook.handlers as orig_handler
-from tornado import web, gen
+import notebook
+from tornado import web
 from traitlets.config import LoggingConfigurable
 from traitlets import Bool, Unicode
+
+
+async def await_if_awaitable(obj):
+    """Convert a non-awaitable object to a coroutine if needed,
+    and await if it was a coroutine.
+
+    Designed to be called on the result of calling a function,
+    when that function could be asynchronous or not.
+    """
+    if inspect.isawaitable(obj):
+        obj = await obj
+    return obj
 
 
 class Appmode(LoggingConfigurable):
@@ -17,6 +31,8 @@ class Appmode(LoggingConfigurable):
     trusted_path = Unicode('', help="Run only notebooks below this path in Appmode.", config=True)
     show_edit_button = Bool(True, help="Show Edit App button during Appmode.", config=True)
     show_other_buttons = Bool(True, help="Show other buttons, e.g. Logout, during Appmode.", config=True)
+    temp_dir = Unicode('', help="Create temporary Appmode notebooks in this directory.", config=True)
+    hidden_temp_files = Bool(True, help="Temporary Appmode notebooks are hidden files.", config=True)
 
 #===============================================================================
 class AppmodeHandler(IPythonHandler):
@@ -32,12 +48,19 @@ class AppmodeHandler(IPythonHandler):
     def show_other_buttons(self):
         return self.settings['appmode'].show_other_buttons
 
+    @property
+    def temp_dir(self):
+        return self.settings['appmode'].temp_dir
+
+    @property
+    def hidden_temp_files(self):
+        return self.settings['appmode'].hidden_temp_files
+
     #===========================================================================
     @web.authenticated
-    def get(self, path):
+    async def get(self, path):
         """get renders the notebook template if a name is given, or
         redirects to the '/files/' handler if the name is not given."""
-
         path = path.strip('/')
         self.log.info('Appmode get: %s', path)
 
@@ -47,10 +70,9 @@ class AppmodeHandler(IPythonHandler):
             raise web.HTTPError(401, 'Notebook is not within trusted Appmode path.')
 
         cm = self.contents_manager
-
         # will raise 404 on not found
         try:
-            model = cm.get(path, content=False)
+            model = await await_if_awaitable(cm.get(path, content=False))
         except web.HTTPError as e:
             if e.status_code == 404 and 'files' in path.split('/'):
                 # 404, but '/files/' in URL, let FilesRedirect take care of it
@@ -65,7 +87,7 @@ class AppmodeHandler(IPythonHandler):
         self.add_header("Cache-Control", "cache-control: private, max-age=0, no-cache, no-store")
 
         # gather template parameters
-        tmp_path = self.mk_tmp_copy(path)
+        tmp_path = await self.mk_tmp_copy(path)
         tmp_name = tmp_path.rsplit('/', 1)[-1]
         render_kwargs = {
             'notebook_path': tmp_path,
@@ -87,52 +109,55 @@ class AppmodeHandler(IPythonHandler):
 
     #===========================================================================
     @web.authenticated
-    @gen.coroutine
-    def post(self, path):
+    async def post(self, path):
         assert self.get_body_arguments("appmode_action")[0] == "delete"
         path = path.strip('/')
         self.log.info('Appmode deleting: %s', path)
 
         # delete session, including the kernel
         sm = self.session_manager
-        if gen.is_coroutine_function(sm.get_session):
-            s = yield sm.get_session(path=path)
-        else:
-            s = sm.get_session(path=path)
-        if gen.is_coroutine_function(sm.delete_session):
-            yield sm.delete_session(session_id=s['id'])
-        else:
-            sm.delete_session(session_id=s['id'])
+
+        s = await await_if_awaitable(sm.get_session(path=path))
+        await await_if_awaitable(sm.delete_session(session_id=s['id']))
 
         # delete tmp copy
         cm = self.contents_manager
-        cm.delete(path)
-        self.finish()
+        await await_if_awaitable(cm.delete(path))
+        await self.finish()
 
     #===========================================================================
-    def mk_tmp_copy(self, path):
+    async def mk_tmp_copy(self, path):
         cm = self.contents_manager
-
         # find tmp_path
-        dirname = os.path.dirname(path)
+        dirname = self.temp_dir or os.path.dirname(path)
+        if dirname and not os.path.exists(dirname):
+            os.makedirs(dirname)
         fullbasename = os.path.basename(path)
         basename, ext = os.path.splitext(fullbasename)
+        if self.hidden_temp_files:
+            basename = '.' + basename
         for i in itertools.count():
-            tmp_path = "%s/.%s-%i%s"%(dirname, basename, i, ext)
+            tmp_path = "%s/%s-%i%s"%(dirname, basename, i, ext)
             if not cm.exists(tmp_path):
                 break
 
         # create tmp copy - allows opening same notebook multiple times
         self.log.info("Appmode creating tmp copy: "+tmp_path)
-        cm.copy(path, tmp_path)
+        await await_if_awaitable(cm.copy(path, tmp_path))
+        return tmp_path
 
-        return(tmp_path)
 
 #===============================================================================
 def load_jupyter_server_extension(nbapp):
     tmpl_dir = os.path.dirname(__file__)
+    notebook_tmpl_dir = os.path.join(os.path.dirname(notebook.__file__), 'templates')
     # does not work, because init_webapp() happens before init_server_extensions()
-    #nbapp.extra_template_paths.append(tmpl_dir) # dows
+    # nbapp.extra_template_paths.append(tmpl_dir) # dows
+
+    # For jupyter server, the notebook templates are not available in the default search paths. This can be addressed
+    # by using --ServerApp.extra_template_paths='***site-packages***\notebook\templates', but this is messy.
+    # To emulate this instead insert the notebook template directory at the start of the searchpath
+    # These will be used last, so the notebook.html resolves, but the page.html is still from jupyter server templates
 
     # For configuration values that can be set server side
     appmode = Appmode(parent=nbapp)
@@ -143,6 +168,7 @@ def load_jupyter_server_extension(nbapp):
     for loader in getattr(rootloader, 'loaders', [rootloader]):
         if hasattr(loader, 'searchpath') and tmpl_dir not in loader.searchpath:
             loader.searchpath.append(tmpl_dir)
+            loader.searchpath.insert(0, notebook_tmpl_dir)
 
     web_app = nbapp.web_app
     host_pattern = '.*$'
